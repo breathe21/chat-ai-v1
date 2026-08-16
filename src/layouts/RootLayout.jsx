33da5ac6dc757e54dc463a3ce0842e93d3316363
breathe21 <tuananhcepdn@gmail.com>
2026-08-16 17:29:28 +0700

    upgrade code

---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Header from './Header'
+import SideBar from './SideBar'
+
+const RootLayout = () => {
+  const [openSideBar, setOpenSideBar] = useState(false);
+
+  return (
+    <div className='h-full'>
+      
+    </div>
+  )
+}
+
+export default RootLayout
--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,37 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import App from './App.jsx'
+import { createBrowserRouter,RouterProvider, } from "react-router-dom";
+import RootLayout from './layouts/RootLayout.jsx';
+import { Provider } from 'react-redux';
+import Home from './pages/Home.jsx'
+import store from './redux/index.js';
+import ChatDetail from './pages/ChatDetail.jsx';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App />
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/chat/info',
+        element: <ChatDetail />
+      },
+      {
+        path: '/chat/:id',
+        element: <Home />
+      }
+    ]
   },
 ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
 )
