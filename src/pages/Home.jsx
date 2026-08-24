c0dc39988ad96eb15836171fed6a1a5d9312e79c
breathe21 <tuananhcepdn@gmail.com>
2026-08-24 10:28:42 +0700

    update phase 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,13 @@ const Home = () => {
         <div className="flex flex-col space-y-5 text-center">
           <div className="space-y-4">
             <p 
-              className="bg-gradient-to-r from-blue-600 to-indigo-400 
+              className="bg-gradient-to-r from-blue-200 to-blue-800 
               text-[40px] text-3xl inline-block text-transparent bg-clip-text">
               Welcome to AI Studio
             </p>
           </div>
           <div className="flex items-center space-x-4 mt-4">
-            <input 
+            <input
               type="text"
               className="w-full p-4 text-primary border border-transparent caret-gray-300 text-md focus:outline-none focus:ring-0 bg-input rounded-2xl"
               placeholder="Nhập nội dung..."
