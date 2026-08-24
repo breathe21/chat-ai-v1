da0700ab5747d47684626b976bc90fabb16502b7
breathe21 <tuananhcepdn@gmail.com>
2026-08-24 09:39:06 +0700

    feat: cap nhat dispatch cho nut them va xoa tro chuyen

--- src/layouts/SideBar.jsx
+++ src/layouts/SideBar.jsx
@@ -1,8 +1,22 @@
 import AddIcon from '@mui/icons-material/Add';
 import ChatIcon from '@mui/icons-material/Chat';
 import DeleteIcon from '@mui/icons-material/Delete';
+import { useDispatch, useSelector } from 'react-redux';
+import { addChat, removeChat } from '../redux/slices/chatSlice';
 
 const SideBar = ({ openSideBar, setOpenSideBar }) => {
+
+  const dispatch = useDispatch();
+  const { data } = useSelector((state) => state.chat);
+
+  const handleNewChat = () => {
+    dispatch(addChat());
+  }
+
+  const handleRemoveChat = (id) => {
+    dispatch(removeChat(id));
+  }
+
   return (
     <>
       {/* Overlay mobile */}
@@ -23,21 +37,31 @@ const SideBar = ({ openSideBar, setOpenSideBar }) => {
         `}
       >
         <div className="mt-20">
-          <button className="w-full px-4 py-2 flex items-center gap-2 bg-gray-600 mb-10 rounded-lg cursor-pointer">
+          <button className="w-full px-4 py-2 flex items-center gap-2 bg-gray-600 mb-10 rounded-lg cursor-pointer"
+            onClick={handleNewChat}
+          >
             <AddIcon className="w-4 h-4" />
             <span>Cuộc trò chuyện mới</span>
           </button>
 
           <div className="space-y-4">
-            <p>Gần đây:</p>
-
-            <div className="flex items-center justify-between p-2 bg-gray-800 rounded-lg">
-              <div className="flex items-center gap-4">
-                <ChatIcon />
-                <p>chat</p>
-              </div>
-
-              <DeleteIcon className="cursor-pointer" />
+            <p>Gần đây</p>
+            {/* Đưa space-y-2 vào đây để các item chat xếp chồng lên nhau */}
+            <div className="flex flex-col space-y-4">
+              {data && data.map((chat) => (
+                <div 
+                  key={chat?.id} 
+                  className="flex items-center justify-between p-2 bg-gray-800 hover:bg-gray-700 rounded-lg transition-colors"
+                >
+                  <div className="flex items-center gap-4">
+                    <ChatIcon />
+                    <p>{chat?.title}</p>
+                  </div>
+                  <button onClick={() => handleRemoveChat(chat.id)}>
+                    <DeleteIcon className="cursor-pointer" />
+                  </button>
+                </div>
+              ))}
             </div>
           </div>
         </div>
